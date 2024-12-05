4599978a882e01dce158c640a442da2d7df5c569
workwithA <workabbasbayat@gmail.com>
2024-12-05 19:23:26 +0330

    DB basics done

--- app/page.js
+++ app/page.js
@@ -1,23 +1,56 @@
 "use client";
 
-import handler from "@/api/newNote";
-import { useState } from "react";
+import handler, { getProps, handleDelete, handleUpdate } from "@/lib/action";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [state, setState] = useState();
+  const [stateAge, setStateAge] = useState();
+  const [result, setResult] = useState();
   const handleSubmit = () => {
     console.log(state);
-    const obj = { title: state };
+    const obj = { title: state, age: stateAge };
     handler(obj);
   };
+
+  useEffect(() => {
+    const func = async () => {
+      try {
+        const res = await getProps();
+        setResult(res);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    func();
+  }, []);
+
   return (
-    <div className="w-full flex justify-around">
-      <input
-        type="text"
-        onChange={(e) => setState(e.target.value)}
-        className="shadow-lg"
-      />
-      <input type="button" value="send" onClick={handleSubmit} />
+    <div className="w-full flex flex-col justify-around">
+      <div className="flex">
+        <input
+          type="text"
+          onChange={(e) => setState(e.target.value)}
+          className="shadow-lg"
+        />
+        <input
+          type="text"
+          onChange={(e) => setStateAge(e.target.value)}
+          className="shadow-lg"
+        />
+        <input type="button" value="send" onClick={handleSubmit} />
+      </div>
+      <div>
+        {result?.map((res) => (
+          <div key={res._id}>
+            <p>{res.title}</p>
+            <p onClick={() => handleDelete(res._id)}>delete</p>
+            <p onClick={() => handleUpdate(res._id, { title: "ha", age: 8 })}>
+              update
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- model/Note.js
+++ model/Note.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
-const noteSchema = new mongoose.Schema({ title: { type: String } });
+const noteSchema = new mongoose.Schema({
+  title: { type: String },
+  age: { type: Number },
+});
 mongoose.models = {};
 module.exports = mongoose.model("Note", noteSchema);
