5edcb370e62ce4d9f7a8d633eeb314aa8722d46e
workwithA <workabbasbayat@gmail.com>
2024-12-05 23:30:51 +0330

    clear for new chapter

--- app/page.js
+++ app/page.js
@@ -1,56 +1,6 @@
-"use client";
-
-import handler, { getProps, handleDelete, handleUpdate } from "@/lib/action";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [state, setState] = useState();
-  const [stateAge, setStateAge] = useState();
-  const [result, setResult] = useState();
-  const handleSubmit = () => {
-    console.log(state);
-    const obj = { title: state, age: stateAge };
-    handler(obj);
-  };
-
-  useEffect(() => {
-    const func = async () => {
-      try {
-        const res = await getProps();
-        setResult(res);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    func();
-  }, []);
-
   return (
-    <div className="w-full flex flex-col justify-around">
-      <div className="flex">
-        <input
-          type="text"
-          onChange={(e) => setState(e.target.value)}
-          className="shadow-lg"
-        />
-        <input
-          type="text"
-          onChange={(e) => setStateAge(e.target.value)}
-          className="shadow-lg"
-        />
-        <input type="button" value="send" onClick={handleSubmit} />
-      </div>
-      <div>
-        {result?.map((res) => (
-          <div key={res._id}>
-            <p>{res.title}</p>
-            <p onClick={() => handleDelete(res._id)}>delete</p>
-            <p onClick={() => handleUpdate(res._id, { title: "ha", age: 8 })}>
-              update
-            </p>
-          </div>
-        ))}
-      </div>
+    <div>
     </div>
   );
 }
--- model/Note.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const noteSchema = new mongoose.Schema({
-  title: { type: String },
-  age: { type: Number },
-});
-mongoose.models = {};
-module.exports = mongoose.model("Note", noteSchema);
